6147552d18df2533918200e5657db469d78609bc
jovanhyw <32458958+jovanhyw@users.noreply.github.com>
2019-10-19 18:46:29 +0800

    Updated a few stuff
    Added netlify file, axios, and veevalidate. Updated ESLint to allow no-console for Netlify Production to build for now

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import ApiService from './services/api.service'
 
 Vue.config.productionTip = false
+
+ApiService.init(process.env.VUE_APP_API_URL)
 
 new Vue({
   router,
